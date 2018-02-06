acadf2832c2d7ecef88fb0df77ff17dd3ddd2cd7
SfTd <j.zolcik@allblue.pl>
2018-02-06 13:41:40 +0100

    Major update.

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 'use strict';
 
-var chalk = require('chalk');
 
-
-module.exports = {
-    error: function() {
-        arguments[0] = chalk.red(arguments[0]);
-        console.error.apply(console, arguments);
-    },
-    log: function() {
-        console.log.apply(console, arguments);
-    },
-    success: function(msg) {
-        arguments[0] = chalk.green(arguments[0]);
-        console.log.apply(console, arguments);
-    },
-    warn: function(msg) {
-        arguments[0] = chalk.yellow(arguments[0]);
-        console.warn.apply(console, arguments);
-    }
-};
+module.exports = require('./lib');
